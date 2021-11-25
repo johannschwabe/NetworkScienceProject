45fcece03a1f22a5c581a345a131ed49b0a82596
Johann Schwabe <johann.schwabe@uzh.ch>
2021-11-25 13:25:50 +0100

    initial Structure

---
+++ InterdependantNetwork.py
@@ -0,0 +1,6 @@
+class InterdependantNetwork:
+    def __init__(self, type, nr_nodes_1, nr_nodes_2, ):
+        self.graph_1 = None
+        self.graph_2 = None
+        self.interconnection = []
+
---
+++ Simulator.py
@@ -0,0 +1,4 @@
+from InterdependantNetwork import InterdependantNetwork
+class Simulator:
+    def __init__(self, network:InterdependantNetwork):
+        self.network = network
