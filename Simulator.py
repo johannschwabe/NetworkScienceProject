4acdc09e7192baecbfa1a818a5d2a3e5a6f28517
thommann <thomasrolf.mannhart@uzh.ch>
2021-12-06 14:45:04 +0100

    fix boggo

--- Simulator.py
+++ Simulator.py
@@ -263,17 +263,3 @@ class Simulator:
             directory = os.path.dirname(__file__)
             path = os.path.join(directory, 'figures', str(name) + ".png")
             plt.savefig(path)
-
-
-# The actual analysis
-
-sim = Simulator()
-# sim.analyse_interdependent_er_augmenting_n()
-# sim.analyse_regular_er_augmenting_n()
-# sim.compare_regular_interdependent_er()
-sim.analyse_inter_networks_augmenting_n()
-sim.analyse_reg_networks_augmenting_n()
-sim.comparison_inter_reg_part2()
-
-
-
