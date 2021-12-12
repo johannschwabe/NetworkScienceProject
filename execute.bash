49079b18bd569b4b202554a0656b61a753c2f5a6
thommann <thomasrolf.mannhart@uzh.ch>
2021-12-12 21:07:51 +0100

    update script

--- execute.bash
+++ execute.bash
@@ -1,13 +1,9 @@
-nohup python3 main.py 20 2 0.56 0.9 15 1000 6 10000 2 1 > ex1.txt &
-# nohup python3 main.py 20 2 0.56 0.9 10 1000 6 50000 2 1 > ex2.txt &
+# ER
+nohup python3 main.py 20 2 0.59 0.625 20 1000 7 0 1 > ex1.txt &
+nohup python3 main.py 20 2 0.59 0.625 20 1000 7 0 2 > ex2.txt &
+nohup python3 main.py 20 2 0.59 0.625 20 1000 7 0 3 > ex3.txt &
 
-nohup python3 main.py 30 2 0.55 0.625 20 100 8 10000 1 1 > ex3.txt &
-nohup python3 main.py 30 2 0.55 0.625 20 1000 5 10000 1 1 > ex9.txt &
-
-
-
-nohup python3 main.py 20 2 0.56 0.9 15 1000 6 10000 2 2 > ex5.txt &
-# nohup python3 main.py 20 2 0.56 0.9 10 1000 6 50000 2 2 > ex6.txt &
-
-nohup python3 main.py 30 2 0.55 0.625 20 100 8 10000 1 2 > ex7.txt &
-nohup python3 main.py 30 2 0.55 0.625 20 1000 5 10000 1 2 > ex10.txt &
+# NS
+nohup python3 main.py 20 2 0.56 0.8 20 0 0 10000 4 > ex4.txt &
+nohup python3 main.py 20 2 0.56 0.8 20 0 0 10000 5 > ex5.txt &
+nohup python3 main.py 20 2 0.56 0.8 20 0 0 10000 6 > ex6.txt &
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from Simulator import Simulator
 import sys
 
-if len(sys.argv) < 10:
-    raise Exception()
+if len(sys.argv) != 9:
+    raise Exception(f"Wrong number of arguments. {len(sys.argv)} / 9")
 nr_created_networks = int(sys.argv[1])
 nr_runs_per_network = int(sys.argv[2])
 range_start = float(sys.argv[3])
@@ -12,17 +12,22 @@ er_start_n = int(sys.argv[6])
 er_nr_steps = int(sys.argv[7])
 nr_nodes = int(sys.argv[8])
 plot_nr = int(sys.argv[9])
-bidirectional = sys.argv[10] == "1"
 
-sim = Simulator(nr_created_networks, nr_runs_per_network, range_start, range_end, range_steps, bidirectional)
+sim = Simulator(nr_created_networks, nr_runs_per_network, range_start, range_end, range_steps)
 
 if plot_nr == 1:
-    # Plt 1
-    sim.analyse_inter_er_augmenting_n(er_start_n, er_nr_steps)
+    sim.analyse_inter_er_augmenting_n(er_start_n, er_nr_steps, True)
+elif plot_nr == 2:
+    sim.analyse_inter_er_augmenting_n(er_start_n, er_nr_steps, False)
+elif plot_nr == 3:
     sim.analyse_reg_er_augmenting_n(er_start_n, er_nr_steps)
-else:
-    # Plt 2
-    sim.analyse_inter_networks_augmenting_n(nr_nodes)
+elif plot_nr == 4:
+    sim.analyse_inter_networks_augmenting_n(nr_nodes, True)
+elif plot_nr == 5:
+    sim.analyse_inter_networks_augmenting_n(nr_nodes, False)
+elif plot_nr == 6:
     sim.analyse_reg_networks_augmenting_n(nr_nodes)
+else:
+    raise Exception("Invalid plot nr.")
 
 sim.save_results()
