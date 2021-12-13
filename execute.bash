a4b9bd96b97949056de77cba168c6f86a90597a8
thommann <thomasrolf.mannhart@uzh.ch>
2021-12-13 08:32:29 +0100

    2

--- execute.bash
+++ execute.bash
@@ -1,9 +1,9 @@
 # ER
-nohup python3 main.py 20 2 0.59 0.625 20 1000 7 0 1 > ex1.txt &
-nohup python3 main.py 20 2 0.59 0.625 20 1000 7 0 2 > ex2.txt &
-nohup python3 main.py 20 2 0.59 0.625 20 1000 7 0 3 > ex3.txt &
+nohup python3 main.py 2 2 0.55 0.65 20 1000 7 0 1 > ex1.txt &
+nohup python3 main.py 2 2 0.55 1.0 20 1000 7 0 2 > ex2.txt &
+nohup python3 main.py 2 2 0.1 0.5 20 1000 7 0 3 > ex3.txt &
 
 # NS
-nohup python3 main.py 20 2 0.56 0.8 20 0 0 10000 4 > ex4.txt &
-nohup python3 main.py 20 2 0.56 0.8 20 0 0 10000 5 > ex5.txt &
-nohup python3 main.py 20 2 0.56 0.8 20 0 0 10000 6 > ex6.txt &
+nohup python3 main.py 2 2 0.55 0.8 20 0 0 50000 4 > ex4.txt &
+nohup python3 main.py 2 2 0.55 1.0 20 0 0 50000 5 > ex5.txt &
+nohup python3 main.py 2 2 0.1 0.5 20 0 0 50000 6 > ex6.txt &
--- playground2.py
+++ playground2.py
@@ -2,9 +2,9 @@ from Simulator import Simulator
 
 nr_created_networks = 2
 nr_runs_per_network = 2
-range_start = 0.6
-range_end = 1.0
-range_steps = 10
+range_start = 0.1
+range_end = 0.5
+range_steps = 9
 
 nr_nodes = 50000
 
@@ -15,12 +15,12 @@ sim = Simulator(nr_created_networks, nr_runs_per_network, range_start, range_end
 
 # ER
 # sim.analyse_inter_er_augmenting_n(er_start_n, er_nr_steps, True)
-sim.analyse_inter_er_augmenting_n(er_start_n, er_nr_steps, False)
-# sim.analyse_reg_er_augmenting_n(er_start_n, er_nr_steps)
+# sim.analyse_inter_er_augmenting_n(er_start_n, er_nr_steps, False)
+sim.analyse_reg_er_augmenting_n(er_start_n, er_nr_steps)
 
 # NS
 # sim.analyse_inter_networks_augmenting_n(nr_nodes, True)
-sim.analyse_inter_networks_augmenting_n(nr_nodes, False)
-# sim.analyse_reg_networks_augmenting_n(nr_nodes)
+# sim.analyse_inter_networks_augmenting_n(nr_nodes, False)
+sim.analyse_reg_networks_augmenting_n(nr_nodes)
 
 sim.save_results()
