7e41d3495e555ebf40b983fab4dccbb93f438c23
thommann <thomasrolf.mannhart@uzh.ch>
2021-12-13 14:51:19 +0100

    plotter

--- HarryPlotter.py
+++ HarryPlotter.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file_path = "results/resultsInterErBidir94399.csv"
+# file_path = "results/resultsInterErBidir98299.csv"
+# file_path = "results/resultsInterErUnidir98326.csv"
+file_path = "results/resultsRegEr92389.csv"
+
+# fig_path = "figures/BidirER"
+# fig_path = "figures/UnidirER"
+fig_path = "figures/RegER"
+
+x_min = 0.4
+x_max = 2.6
+
+x_label = "p<k>"
+# x_label = "p"
 
 dataframe = pd.read_csv(file_path)
 
@@ -12,11 +24,12 @@ for col in dataframe.columns[1:-1]:
 plt.legend()
 
 # Title
-plt.title("Interdependent Erdos Renyi networks", fontsize=9)
+# plt.title("Interdependent Bidirectional Erdos Renyi networks", fontsize=9)
+plt.title("Independent Erdos Renyi networks", fontsize=9)
 
 # x-axes
-plt.xlim(0.0, 4.0)
-plt.xlabel("p<k>")
+plt.xlim(x_min, x_max)
+plt.xlabel(x_label)
 # plt.xlim(0.0, 1.0)
 # plt.xlabel("p")
 
@@ -24,4 +37,4 @@ plt.xlabel("p<k>")
 plt.ylim(0.0, 1.0)
 plt.ylabel("P Infinity")
 
-plt.show()
+plt.savefig(fig_path + f"-{x_min}-{x_max}".replace(".", "_") + ".png")
