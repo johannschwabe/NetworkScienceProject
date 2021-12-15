56089694e991d29308ed27bb913f96c61960f1be
thommann <thomasrolf.mannhart@uzh.ch>
2021-12-15 13:25:39 +0100

    connected

--- execute.bash
+++ execute.bash
@@ -1,9 +1,9 @@
 # ER
 nohup python3 main.py 10 2 0.5 0.7 21 1000 5 0 1 > ex1.txt &
 nohup python3 main.py 10 2 0.5 0.9 21 1000 5 0 2 > ex2.txt &
-nohup python3 main.py 10 2 0.1 0.6 21 1000 5 0 3 > ex3.txt &
+#nohup python3 main.py 10 2 0.1 0.6 21 1000 5 0 3 > ex3.txt &
 
 # NS
 nohup python3 main.py 5 2 0.5 0.8 16 0 0 50000 4 > ex4.txt &
 nohup python3 main.py 5 2 0.5 0.9 17 0 0 50000 5 > ex5.txt &
-nohup python3 main.py 5 2 0.1 0.6 16 0 0 50000 6 > ex6.txt &
+#nohup python3 main.py 5 2 0.1 0.6 16 0 0 50000 6 > ex6.txt &
